55f7064c0f7f4a4c052d30b9a20bb88cf6497abc
teachmecoding <lars.han@outlook.com>
2019-01-09 19:48:16 +0100

    Commit and push test 1

---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+    // What the camera will follow
+    public Transform target;
+
+    // Other camera properties
+    Vector3 velocity = Vector3.zero;
+    public float smoothTime = 0.2f;
+
+    //
+    // Clamping variables
+    //
+    //enable and Max Y value
+    public bool yMaxEnabled;
+    public float yMaxValue;
+
+    //enable and Minimum Y Value
+    public bool yMinEnabled;
+    public float yMinValue;
+
+    //enable and Max X value
+    public bool xMaxEnabled;
+    public float xMaxValue;
+
+    //enable and Minimum Y Value
+    public bool xMinEnabled;
+    public float xMinValue;
+
+    void FixedUpdate() 
+    {
+        Vector3 targetPos = target.position;
+        targetPos.z = transform.position.z;
+
+        //
+        // Clamps
+        //
+        // Vertical restrictions
+        if (yMaxEnabled && yMinEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, yMinValue, yMaxValue);
+        }
+        else if (yMinEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y,yMinValue,target.position.y);
+        }
+        else if (yMaxEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, target.position.y, yMaxValue);
+        }
+
+        // Horizontal restrictions
+        if (xMaxEnabled && xMinEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, xMinValue, xMaxValue);
+        }
+        else if (xMinEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x,xMinValue,target.position.x);
+        }
+        else if (xMaxEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, target.position.x, xMaxValue);
+        }
+        
+
+        // Moving the camera    
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTest : MonoBehaviour {
+
+[SerializeField]private float movementSpeed = 100.0f;
+	private Rigidbody2D rigidbody2D;
+	private float horizontalMove = 0f;
+	private float verticalMove = 0f;
+	
+	//private Animator animator;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	// This is where we should get user input
+	void Update () 
+	{
+		horizontalMove 	= Input.GetAxisRaw("Horizontal"); // A or D button
+		verticalMove 	= Input.GetAxisRaw("Vertical"); // W or S button
+	}
+
+	private void FixedUpdate() 
+	{
+		rigidbody2D.velocity = new Vector2(0,0); // reset velocity each frame
+
+		if(horizontalMove > 0) // D button, move right
+		{
+			rigidbody2D.velocity = new Vector2(movementSpeed * Time.deltaTime,rigidbody2D.velocity.y);
+		}
+		else if(horizontalMove < 0) // A button, move left
+		{
+			rigidbody2D.velocity = new Vector2(-movementSpeed * Time.deltaTime,rigidbody2D.velocity.y);
+		}
+
+		if(verticalMove > 0) // W button, move up
+		{
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,movementSpeed * Time.deltaTime);
+		}
+		else if(verticalMove < 0) // S button, move down
+		{
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,-movementSpeed * Time.deltaTime);
+		}
+	}
+}
