352005107134262dc184f8bf789b435bb6a65598
teachmecoding <lars.han@outlook.com>
2019-01-10 18:14:04 +0100

    NEw commit

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
     public bool isDead;
-    public int player_HealthPoints;
+    public int player_HealthPoints = 100;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player_HealthPoints = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionStay2D(Collision2D other) 
+    {
+        if(other.gameObject.tag == "Unfriendly")
+        {
+            player_HealthPoints--; 
+        }
     }
 }
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    PlayerController playerController;
+
+    public Text playerHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHP.text = "HP: " + playerController.player_HealthPoints.ToString();
+    }
+}
