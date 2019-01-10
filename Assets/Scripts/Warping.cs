cace030ef6ea7b9e4f8761bc04547df6d296eaa7
Pål Bøckmann <plbockmann@gmail.com>
2019-01-10 18:52:27 +0100

    warping

---
+++ Assets/Scripts/Warping.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Warping : MonoBehaviour
+{
+    public Transform warpTarget;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(0);
+        //Debug.Log("An object collided");
+        //other.gameObject.transform.position = warpTarget.position;
+        //Camera.main.transform.position = warpTarget.position;
+    }
+}
