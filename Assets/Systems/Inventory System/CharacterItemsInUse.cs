247ef6240f4c0f6fb5414ff81b238c0c161bfe73
teachmecoding <lars.han@outlook.com>
2019-03-12 20:13:10 +0100

    Started on writing script for saving characters armor and weapons put on

---
+++ Assets/Systems/Inventory System/CharacterItemsInUse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character Wareables",menuName = "Own Menu/Inventory/New Character Wearables")]
+public class CharacterItemsInUse : ScriptableObject
+{
+    public Item_SO helmet;
+    public Item_SO chest;
+    public Item_SO pants;
+    public Item_SO gloves;
+    public Item_SO boots;
+    public Item_SO melee;
+    public Item_SO ranged;
+
+}
